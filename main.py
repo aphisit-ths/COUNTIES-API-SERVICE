ffd78de6338e0a6c745ff03770ad50d53d0ec858
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2022-04-14 03:13:48 +0700

    add readme / cors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import json
 
 def read_data():
@@ -6,27 +8,38 @@ def read_data():
         data = json.load(f)
     return data
 
-    
-
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     data = read_data()
     return data
 
-@app.get("/{id}")
-async def search(id):
+@app.get("/{country}")
+async def search(country):
     with open("./database/data.json",encoding='utf-8') as f:
         input_dict = json.load(f)
     # Filter python objects with list comprehensions
-    output_dict = [x for x in input_dict if x['name'] == id]
-    
-    if(len(output_dict) == 0):
-        return {"errors":"please ! try correct country name "}
+    output_dict = [x for x in input_dict if x['name'].lower() == country.lower()]
     
     # Transform python object back into json
     output_json = json.dumps(output_dict)
     parsed = json.loads(output_json)
-    
+        
     return parsed
